891a5d1406fa994ee6a63fec93d4c61e71984920
kartheekgj <kartheekgj@gmail.com>
2014-07-17 15:37:30 +0530

    common config its my opensource php mongodb config

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+
+DEFINE('USERNAME','kartheekgj');
+DEFINE('PASSWORD','gjkgjk');
+DEFINE('DBNAME','gj');
+DEFINE('COLLECTION_NAME','shorturl');
+
+$conn_url = "mongodb://".USERNAME.":".PASSWORD."@ds029837.mongolab.com:29837/gj";
